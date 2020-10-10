d94c1a841410c03db015d19800758784db029998
Denys Zhereb <zherebd17@gmail.com>
2020-10-10 23:24:46 +0300

    connectiont ti database

--- app.js
+++ app.js
@@ -4,5 +4,18 @@ const mongoose = require('mongoose')
 
 const app = express()
 const PORT = config.get('port') || 5000
-
-app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+async function start(){
+    try{
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        })
+        app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`))
+    }
+    catch(e){
+        console.log('Server error...', e.message)
+        process.exit(1);
+    }
+}
+start()
