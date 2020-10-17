c4958a10791fbab1eaa3277242031f180d107b16
Irqa <iryna.lyshenko@gmail.com>
2020-10-17 23:06:15 +0300

    fixing authorization

--- client/src/hooks/http.hook.js
+++ client/src/hooks/http.hook.js
@@ -3,11 +3,15 @@ import {useState,useCallback} from 'react'
 
 export const useHttp = () =>{
   const [loading, setLoading] = useState(false)
-  const [error, setError] = useState({initialState:null})
+  const [error, setError] = useState(null)
 
   const request = useCallback(async (url, method ='GET', body = null, headers = {}) => {
     setLoading(true)
     try {
+      if (body){
+        body=JSON.stringify(body)
+        headers['Content-Type'] = 'application/json'
+      }
       const response = await fetch(url,{method,body,headers})
       const data = await response.json()
 
