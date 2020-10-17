d2cd815771c3c2b89bf7c97a4b0b89f2bd810562
Denys Zhereb <zherebd17@gmail.com>
2020-10-17 20:09:35 +0300

    auth routing is done

--- Routes/auth.routes.js
+++ Routes/auth.routes.js
@@ -1,17 +1,69 @@
 const {Router} = require('express')
 const router = Router()
+const bcrypt = require('bcrypt')
+const User = require('../Models/User')
+const {check, validationResult} = require('express-validator')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 
 //   api/auth/register
-router.post('/register', async (req, res) => {
+const validationRegister = [
+    check('email', 'Некоректний email').isEmail(),
+    check('password', 'Довжина паролю повина бути більша 6 символів').isLength({min: 6})
+]
+router.post('/register', validationRegister , async (req, res) => {
     try{
-        const {email, password} = req.body
+        const errors = validationResult(req)
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors: errors.array(),
+                message: "Не коректні дані"
+            })
+        }
+        const {email, password, login} = req.body
+        const candidate = await User.findOne(email)
+        if(condidate){
+            return res.status(400).json({message: "Користувач з наним email вже існує"})
+        }
+        const hashedpassword = await bcrypt.hash(password, 12)
+        const user = new User({email, login, hasedpassword}) 
+        await user.save()
+        res.json({massage: "Користувач створений"})
     }catch(e){
         res.status(500).json({messsge: "Помилка сервера... Спробуйте зноу"})
     }
 })
 
 //   api/auth/login
-router.post('/login', async (req, res) => {
+const validationLogin = [
+    check('email', 'Некоректний email').isEmail()
+]
+router.post('/login', validationLogin, async (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        return res.status(400).json({
+            errors: errors.array(),
+            message: "Не коректні дані"
+        })
+    }
+    const {email, password} = req.body
+    const user = await User.findOne(email)
+    if(!user){
+        return res.status(400).json({message: "Користувача з даним  email не існує"})
+    }
+
+    const isMatch = await bcrypt.compare(user.password, bcrypt.hash(password, 12))
+    if(!isMatch){
+        return res.status(400).json({message: "Не правилтний пароль"})
+    }
+    const token = jwt.sign(
+        {userId: user.id},
+        config.get('jwtSecret'),
+        {expiresIn: '1h' }
+    )
+    res.json({
+        token, userId: user.id
+    })
 
 })
 
