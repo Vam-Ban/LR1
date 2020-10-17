acde9ee28c42b7a650913aaca79c3cda8e91077d
Irqa <iryna.lyshenko@gmail.com>
2020-10-17 21:33:37 +0300

    building authtorization/registration

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -1,6 +1,23 @@
-import React from "react"
+import React, {useState} from "react"
+import {useHttp} from '../hooks/http.hook'
 
 export const AuthPage = () =>{
+  const {loading, error, request} = useHttp()
+  const [form, setForm] = useState({initialState:{
+    email: '', password: ''
+  }})
+
+  const changeHandler = event => {
+    setForm({...form, [event.target.name]: event.target.value})
+  }
+
+  const registerHandler = async () => {
+    try {
+      const data = await request('/api/auth/register','POST',{...form})
+      console.log('Data', data);
+    } catch (e) {}
+  }
+
   return(
     <div className="row">
       <div className="col s6 offset-s3">
@@ -8,12 +25,40 @@ export const AuthPage = () =>{
         <div className="card blue darken-1">
           <div className="card-content white-text">
             <span className="card-title">Авторизація</span>
-            <div>
+            <div className="input-field">
+              <input
+                placeholder="Введіть ваш email"
+                id="email"
+                type="text"
+                name="email"
+                className="yellow-input"
+                onChange={changeHandler} />
+              <label htmlFor="email">Email</label>
+            </div>
+            <div className="input-field">
+              <input
+                placeholder="Введіть ваш пароль"
+                id="password"
+                type="password"
+                name="password"
+                className="yellow-input"
+                onChange={changeHandler} />
+              <label htmlFor="password">Пароль</label>
             </div>
           </div>
           <div className="card-action">
-          <button className="btn yellow darken-4" style={{marginRight: 10}}>Увійти</button>
-          <button className="btn grey lighten-1 black-text">Зареєструватись</button>
+            <button
+              className="btn yellow darken-4"
+              style={{marginRight: 10}}
+              disabled={loading}>
+            Увійти
+            </button>
+            <button
+              className="btn grey lighten-1 black-text"
+              onClick={registerHandler}
+              disabled={loading}>
+            Зареєструватись
+            </button>
           </div>
         </div>
       </div>
