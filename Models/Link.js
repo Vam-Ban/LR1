8608e26a94245e339c9266e2b3e57d4d48f9f65a
Denys Zhereb <zherebd17@gmail.com>
2020-10-18 23:46:25 +0300

    business logic is done

--- Models/Link.js
+++ Models/Link.js
@@ -4,7 +4,7 @@ const schema = new Schema({
     to: { type: String, required: true, unique: true },
     code: { type: String, required: true, unique: true },
     date: { type: Date, default: Date.now },
-    clickes: { type: Number, default: 0 },
+    clicks: { type: Number, default: 0 },
     owner: { type: Types.ObjectId, ref: 'User' }
 })
 module.exports = model('Link', schema)
