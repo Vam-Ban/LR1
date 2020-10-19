8d6fa8b4b1d1d3305aa3d4be70a1ca0705859cbe
Avgustin <cubbot97@gmail.com>
2020-10-19 09:48:26 +0300

    done

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ app.use(express.json({extended: true}))
 
 app.use('/api/auth', require('./Routes/auth.routes'))
 app.use('/api/link', require('./Routes/link.routes'))
-app.use('/t', require('./Routes/redirect.routes'))
 
 const PORT = config.get('port') || 5000
 async function start(){
