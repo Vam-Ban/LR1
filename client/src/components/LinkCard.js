4d01f6f2b063235fdc581ebf5aa3e2427de833a9
Denys Zhereb <zherebd17@gmail.com>
2020-10-20 21:19:41 +0300

    Setting

--- client/src/components/LinkCard.js
+++ client/src/components/LinkCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const LinkCard = ({ link }) => {
   return (
     <>
-      <h2>Ссылка</h2>
+      <h2>Посилання</h2>
 
       <p>Ваше посилання: <a href={link.to} target="_blank" rel="noopener noreferrer">{link.to}</a></p>
       <p>Звідки: <a href={link.from} target="_blank" rel="noopener noreferrer">{link.from}</a></p>
